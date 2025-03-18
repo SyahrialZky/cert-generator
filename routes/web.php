4db641e8710d2a23c52d551689baad932491c9dc
muhBilal <mb336450@gmail.com>
2025-03-18 11:04:58 +0700

    tmp

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pages.checker.index');
-});
+})->name('home');
 
 
 Route::controller(AuthController::class)->group(function () {
