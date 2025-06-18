7bc6b01a5aac088238a5c49e88d5301980e4ae5b
muhBilal <mb336450@gmail.com>
2025-06-18 11:23:02 +0700

    tmp docker

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-    plugins: [
+  server: {
+        host: true, 
+        port: 5173, 
+    },
+     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
